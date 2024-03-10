90c7b593728deadf93c7962a0e1dbf239101043d
Nick Marks <nickmarks00@gmail.com>
2024-03-10 17:00:33 +1100

    Add train function
    This takes a simple dataset of images and actions and runs a short
    training stint on the dataset.
    
    This is simply a proof of concept that the pipeline *works* - it is not
    intended to be good.

---
+++ train.py
@@ -0,0 +1,41 @@
+import numpy as np
+from d3rlpy.algos import IQLConfig
+from d3rlpy.dataset import MDPDataset, FrameStackTransitionPicker
+
+# from d3rlpy.metrics import TDErrorEvaluator, EnvironmentEvaluator
+
+from datasetFactory import DatasetFactory
+
+
+PATH_TO_DATASET = "/home/chumbers/Downloads/"
+
+
+def main() -> None:
+
+    experiment_name = "iql_pibot_mock"
+
+    raw_dataset = DatasetFactory(PATH_TO_DATASET)
+
+    dataset = MDPDataset(
+        observations=raw_dataset.observations,
+        actions=np.random.random((100, 2)),
+        rewards=raw_dataset.rewards,
+        terminals=raw_dataset.terminals,
+        transition_picker=FrameStackTransitionPicker(n_frames=4),
+    )
+
+    iql = IQLConfig().create(device="cuda:0")
+    iql.build_with_dataset(dataset)
+
+    # Train the model
+    iql.fit(
+        dataset,
+        n_steps=1000,
+        n_steps_per_epoch=100,
+        save_interval=10,
+        experiment_name=experiment_name,
+    )
+
+
+if __name__ == "__main__":
+    main()
