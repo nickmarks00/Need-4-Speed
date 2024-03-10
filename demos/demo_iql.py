592a68065206e97abdcaa7054f3962fd5f3bce8f
Nick Marks <nickmarks00@gmail.com>
2024-03-10 16:58:21 +1100

    Fix issue with IQL demo

--- demos/demo_iql.py
+++ demos/demo_iql.py
@@ -10,33 +10,31 @@ import os
 if __name__ == "__main__":
     experiment_name = "iql_hopper_medium_v2"
 
-    os.system(f'rm -rf d3rlpy_logs/{experiment_name}')
+    os.system(f"rm -rf d3rlpy_logs/{experiment_name}")
 
     # List of available tasks is avaiable here: https://github.com/Farama-Foundation/d4rl/wiki/Tasks
-    dataset, env = get_d4rl("hopper-medium-v2") 
+    dataset, env = get_d4rl("hopper-medium-v2")
 
     # Using the vanilla config settings and placing model on GPU
     # Refer to https://d3rlpy.readthedocs.io/en/latest/references/algos.html#d3rlpy.algos.iql.IQL for more details on parameters
     iql = IQLConfig().create(device="cuda:0")
 
     iql.build_with_dataset(dataset)
+    print(iql.get_action_type())
 
     td_error_evaulator = TDErrorEvaluator(episodes=dataset.episodes)
     env_evaluator = EnvironmentEvaluator(env)
     rewards = env_evaluator(iql, dataset=None)
 
-    # Train the model 
+    # Train the model
     iql.fit(
-            dataset,
-            n_steps=10000,
-            n_steps_per_epoch=1000,
-            save_interval=5,
-            experiment_name=experiment_name,
-            with_timestamp=False,
-            evaluators = {
-                'td_error': td_error_evaulator,
-                'environment': env_evaluator
-                }
+        dataset,
+        n_steps=10000,
+        n_steps_per_epoch=1000,
+        save_interval=5,
+        experiment_name=experiment_name,
+        with_timestamp=False,
+        evaluators={"td_error": td_error_evaulator, "environment": env_evaluator},
     )
 
-    os.system(f'd3rlpy plot d3rlpy_logs/{experiment_name}/environment.csv')
+    os.system(f"d3rlpy plot d3rlpy_logs/{experiment_name}/environment.csv")
