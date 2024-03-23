625d68bce90c80ecb91985f37e4f00be68aee17a
Nick Marks <nickmarks00@gmail.com>
2024-03-24 09:52:20 +1100

    Update reward functions for testing v1 model

--- utils/rewards.py
+++ utils/rewards.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import statistics
 from typing import Tuple
 
 
@@ -12,13 +13,13 @@ class RewardHandler:
     """
 
     def __init__(self) -> None:
-        self.smoothness_buffer = Buffer(2)
-        self.pose_buffer = Buffer(3)
+        self.smoothness_buffer = Buffer(buffer_dim=2)
+        self.pose_buffer = Buffer(buffer_dim=3)
         self.weights = {
-            "smoothness": 0.01,
-            "pose_pos": 0.05,
+            "smoothness": 0.1,
+            "pose_pos": 0.01,
             "pose_theta": 0.1,
-            "track": 0.5,
+            "track": 0.1,
         }
 
     def reward(
@@ -36,30 +37,21 @@ class RewardHandler:
 
     def reward_smoothness(self, l_vel: int, r_vel: int) -> float:
         self.smoothness_buffer.push([l_vel, r_vel])
-        l_vel_avg = np.mean(self.smoothness_buffer[0])
-        r_vel_avg = np.mean(self.smoothness_buffer[1])
+        l_vel_avg = statistics.fmean(self.smoothness_buffer[0])
+        r_vel_avg = statistics.fmean(self.smoothness_buffer[1])
 
-        try:
-            reward = self.weights["smoothness"] * (
-                1 / math.sqrt(abs(l_vel - l_vel_avg))
-                + 1 / math.sqrt(abs(r_vel - r_vel_avg))
-            )
-        except ZeroDivisionError:
-            if math.sqrt((l_vel_avg - l_vel) ** 2 + (r_vel_avg - r_vel) ** 2 < 1e-2):
-                reward = 0.1
-            else:
-                reward = 0
-
-        return reward
+        return self.weights["smoothness"] * math.exp(
+            -1 * (((l_vel - l_vel_avg) / 10) ** 2 + ((r_vel - r_vel_avg) / 10) ** 2)
+        )
 
     def reward_pose(self, x: float, y: float, theta: float) -> float:
-        x_avg = np.mean(self.pose_buffer[0])
-        y_avg = np.mean(self.pose_buffer[1])
-        theta_avg = np.mean(self.pose_buffer[2])
+        x_avg = statistics.fmean(self.pose_buffer[0])
+        y_avg = statistics.fmean(self.pose_buffer[1])
+        theta_avg = statistics.fmean(self.pose_buffer[2])
 
         reward = self.weights["pose_pos"] * (
-            math.exp(-1 * ((x - x_avg) ** 2 + (y - y_avg) ** 2))
-        ) + self.weights["pose_theta"] * math.exp(-1 * (theta - theta_avg) ** 2)
+            math.exp(-1 * (1000 * (x - x_avg) ** 2 + 1000 * (y - y_avg) ** 2))
+        ) + self.weights["pose_theta"] * math.exp(-1 * 1000 * (theta - theta_avg) ** 2)
         return reward
 
     def reward_track(self, img: np.ndarray) -> float:
@@ -73,9 +65,11 @@ class RewardHandler:
                 pixel = img[i, j]
                 if np.linalg.norm(pixel) < 150 and np.max(pixel) - np.min(pixel) < 10:
                     grey_pixels += 1
-
         try:
-            return self.weights["track"] * math.log(grey_pixels) / img.shape[0]
+            print(grey_pixels)
+            ratio = grey_pixels / (img.shape[0] * img.shape[1] * img.shape[2])
+            print(ratio)
+            return self.weights["track"] * math.exp(100 * (ratio) ** 3)
         except ZeroDivisionError:
             return 0
 
@@ -88,13 +82,13 @@ class Buffer:
     def __init__(self, buffer_dim, buffer_size=3) -> None:
         self.capacity: int = buffer_size
         self.dim: int = buffer_dim
-        self.buffer: np.ndarray = np.array([[0] for _ in range(self.dim)])
+        self.buffer = [[0] for _ in range(self.dim)]
 
     def push(self, vals) -> None:
         if self.is_full():
             self.pop()
         for i in range(self.dim):
-            np.append(self.buffer[i], vals[i])
+            self.buffer[i].append(vals[i])
 
     def pop(self) -> None:
         for i in range(self.dim):
@@ -113,3 +107,6 @@ class Buffer:
 
     def __setitem__(self, _, vals):
         self.push(vals)
+
+    def __str__(self):
+        return self.buffer.__str__()
