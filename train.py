0b3c247c1ec9354e945d8e6ba87333590b175dc3
Nick Marks <nickmarks00@gmail.com>
2024-03-26 13:50:53 +1100

    Add reward, action and observatin scalers

--- train.py
+++ train.py
@@ -2,6 +2,11 @@
 import os
 from d3rlpy.algos import IQLConfig
 from d3rlpy.dataset import MDPDataset
+from d3rlpy.preprocessing import (
+    StandardObservationScaler,
+    MinMaxActionScaler,
+    StandardRewardScaler,
+)
 import torch
 
 # Local imports
@@ -12,8 +17,6 @@ from utils.file_handler import next_path
 def main() -> None:
 
     path_to_dataset = os.path.join(os.getcwd(), "output")
-
-    experiment_name = "iql"
 
     device = "cuda:0" if torch.cuda.is_available() else None
 
@@ -27,16 +30,25 @@ def main() -> None:
         # transition_picker=FrameStackTransitionPicker(n_frames=4),
     )
 
-    iql = IQLConfig().create(device=device)
+    # Pass scalers so d3rlpy normalises data
+    observation_scaler = StandardObservationScaler()
+    action_scaler = MinMaxActionScaler()
+    reward_scaler = StandardRewardScaler()
+
+    iql = IQLConfig(
+        observation_scaler=observation_scaler,
+        action_scaler=action_scaler,
+        reward_scaler=reward_scaler,
+    ).create(device=device)
     iql.build_with_dataset(dataset)
 
     print("\n=========================================")
     print("Training IQL...")
-    print("=========================================")
-    # Train the model
+    print("=========================================\n")
 
-    epochs = 250
-    n_steps_per_epoch = 250
+    epochs = 1
+    n_steps_per_epoch = 50
+    experiment_name = f"iql-straight-{epochs}epochs-{n_steps_per_epoch}steps"
     iql.fit(
         dataset,
         n_steps=n_steps_per_epoch * epochs,
@@ -51,7 +63,11 @@ def main() -> None:
         print("\nCreated models/ directory")
 
     model_path = next_path("models/iql-%s.d3")
+    # policy_path = next_path("models/iql_sp-%s.pt")
+    # p_model_path = next_path("models/iql_sm-%s.pt")
     iql.save(model_path)
+    # iql.save_policy(policy_path)
+    # iql.save_model(p_model_path)
     print(f"\nModel saved to {model_path}")
 
 
